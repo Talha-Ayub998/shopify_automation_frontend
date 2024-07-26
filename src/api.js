f7aab89cea17f17ede19a2cdfe6477e12aae7286
Talha-Ayub998 <talha.ayub@qbatch.com>
2024-07-26 19:21:36 +0500

    Add csrf header

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-
-
+// Base URL for your API
 const BASE_URL = process.env.REACT_APP_API_URL;
+
 const api = axios.create({
     baseURL: BASE_URL,
 });
@@ -14,6 +14,13 @@ api.interceptors.request.use((config) => {
     if (token) {
         config.headers['Authorization'] = `Token ${token}`; // Use 'Token' for Django Knox
     }
+
+    // Retrieve CSRF token from cookies and add it to the request headers
+    const csrfToken = getCookie('csrftoken'); // Ensure you have a function to get the CSRF token
+    if (csrfToken) {
+        config.headers['X-CSRFToken'] = csrfToken; // Add CSRF token to headers
+    }
+
     return config;
 }, (error) => Promise.reject(error));
 
@@ -29,5 +36,21 @@ api.interceptors.response.use(
         return Promise.reject(error);
     }
 );
+
+// Function to get CSRF token from cookies
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
 
 export default api;
